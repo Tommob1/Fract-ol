1541fdc9f79af69a3894ff7b88692080c9f286ab
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-09-19 14:30:07 +0930

    Added changes to code

---
+++ fract_ol.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract-ol.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 15:31:59 by btomlins          #+#    #+#             */
+/*   Updated: 2023/09/04 15:32:19 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACT_OL_H
+#   define FRACT_OL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mlx.h>
+#include "libft/libft.h"
+
+#endif
