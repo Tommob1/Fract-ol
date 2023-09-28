82f607bead4d57475b6c0a5eb7503c2cc1d03bff
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-09-28 13:34:35 +0930

    Added changes to file

--- fractol.c
+++ fractol.c
@@ -14,8 +14,15 @@
 
 int main(int argc, char **argv)
 {
-    if (argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
+    if (argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10) //COMPLETE
+        || argc == 4 && !ft_strncmp(argv[1], "julia", 5))
     {
         
     }
+    else
+    {
+        ft_putstr_fd(ERROR_MESSAGE, STDERR_FILENO); //COMPLETE
+        exit(EXIT_FAILURE);
+    }
+    
 }
