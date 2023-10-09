290ad53fa93cfbfcaae944903222f07847aff5b9
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-10-09 16:55:29 +1030

    Renamed header file

---
+++ fractol.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract-ol.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 15:31:59 by btomlins          #+#    #+#             */
+/*   Updated: 2023/09/04 15:32:19 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+#   define FRACTOL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "minilibx_macos/mlx.h"
+#include "libft/libft.h"
+
+#endif
