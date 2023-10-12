8934773454ea96bebdec36add4c5ea17d98922a9
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-10-12 16:23:40 +1030

    Added changes to code

--- init.c
+++ init.c
@@ -15,5 +15,10 @@
 void    fractal_init(t_fractal *fractal)
 {
     fractal->mlx_connection = mlx_init();
-    
+    if (fractal->mlx_connection == NULL)
+        malloc_error();
+    fractal->mlx_window = mlx_new_window(fractal->mlx_connection,
+                                        WIDTH,
+                                        HEIGHT, 
+                                        fractal->name);
 }
