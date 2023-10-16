c709d651a7f89a63ad2c6d0a27beca6059f3d0e0
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-10-16 15:44:06 +1030

    Added changes to code

--- fractol.h
+++ fractol.h
@@ -17,7 +17,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "minilibx_macos/mlx.h"
+#include "minilibx/mlx.h"
 #include "libft/libft.h"
 
 #define ERROR_MESSAGE "Please enter \
