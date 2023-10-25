b496bd08fbd4a80a2209e09783c987464e868fd2
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-10-25 13:10:19 +1030

    Added changes to code

--- math_utils.c
+++ math_utils.c
@@ -18,3 +18,12 @@ double old_min, double old_max)
     return (new_max - new_min) * (unscaled_num - old_min) / 
     (old_max - old_min) + new_min;
 }
+
+t_complex   sum_complex(t_complex z1, t_complex z2)
+{
+    t_complex   result;
+
+    result.x = z1.x + z2.x;
+    result.y = z1.y + z2.y;
+    return result;
+}
