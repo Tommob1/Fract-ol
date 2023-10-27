302ccbe02a73d42cc387b3cb518e38ef791dc786
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-10-27 16:20:26 +1030

    Added changes to code

--- fractol.h
+++ fractol.h
@@ -26,6 +26,23 @@
 #define WIDTH   800
 #define HEIGHT  800
 
+// COLORS
+#define BLACK       0x000000
+#define WHITE       0xFFFFFF
+#define RED         0xFF0000
+#define GREEN       0x00FF00
+#define BLUE        0x0000FF
+
+// Psychedelic colors
+#define MAGENTA_BURST   0xFF00FF
+#define LIME_SHOCK      0xCCFF00
+#define NEON_ORANGE     0xFF6600
+#define PSYCHEDELIC_PURPLE 0x660066
+#define AQUA_DREAM      0x33CCCC
+#define HOT_PINK        0xFF66B2
+#define ELECTRIC_BLUE   0x0066FF
+#define LAVA_RED        0xFF3300
+
 typedef struct  s_complex
 {
     double  x;
