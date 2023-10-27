7f047b50106b040b40c32ca826c039ad619e41df
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-10-27 16:10:56 +1030

    Added changes to code

--- render.c
+++ render.c
@@ -16,23 +16,28 @@ void	handle_pixel(int x, int y, t_fractal *fractal)
 {
     t_complex	z;
     t_complex	c;
-
+    int         i;
+    int         color;
+    
+    i = 0;
     z.x = 0.0;
     z.y = 0.0;
 
 	c.x = map(x, -2, +2, 0, WIDTH);
 	c.y = map(y, +2, -2, 0, HEIGHT);
 
-    while ()
+    while (i < fractal->iterations_definition)
     {
-        z = sum_complex(square_complex(z), c)
+        z = sum_complex(square_complex(z), c);
 
         if ((z.x * z.x) + (z.y * z.y) > fractal->escape_value)
         {
+            color = map(i, BLACK, WHITE, 0, fractal->iterations_definition);
             my_pixel_put();
             return;
         }
     }
+
 }
 
 void	fractal_render(t_fractal *fractal)
